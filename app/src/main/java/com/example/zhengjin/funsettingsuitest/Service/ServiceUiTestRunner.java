0c723bdaecd04281ad5851677dfc0c4425cc751a
Vieira-zj <zhengjin@fun.tv>
2016-08-23 21:21:47 +0800

    Run shell instrument command in ServiceUiTestRunner.java

--- app/src/main/java/com/example/zhengjin/funsettingsuitest/Service/ServiceUiTestRunner.java
+++ app/src/main/java/com/example/zhengjin/funsettingsuitest/Service/ServiceUiTestRunner.java
@@ -1,10 +1,12 @@
 package com.example.zhengjin.funsettingsuitest.Service;
 
 import android.app.IntentService;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
+
+import com.example.zhengjin.funsettingsuitest.Utils.ShellUtils;
+
+import java.util.Locale;
 
 public class ServiceUiTestRunner extends IntentService {
 
@@ -23,13 +25,26 @@ public class ServiceUiTestRunner extends IntentService {
     protected void onHandleIntent(Intent intent) {
         String testPackage = intent.getExtras().getString("pkgName");
         String testRunner = intent.getExtras().getString("testRunner");
-        Log.d(TAG, String.format("The intent arguments: %s/%s", testPackage, testRunner));
 
-        // TODO: 2016/8/18 run instrument test
-        Bundle arguments = new Bundle();
-        arguments.putString("debug", "false");
-        arguments.putString("class",
-                "com.example.zhengjin.funsettingsuitest.testcases.TestPlayingFilm#testDemo");
-        this.startInstrumentation(new ComponentName(testPackage, testRunner), null, arguments);
+        String commandInst = "am instrument -w -r";
+        String commandExtraDebug = "-e debug false";
+        String commandExtraClass =
+                "-e class com.example.zhengjin.funsettingsuitest.testcases.TestPlayingFilm#testDemo";
+        String commandRunner = String.format(Locale.getDefault(), "%s/%s", testPackage, testRunner);
+        String command = String.format(Locale.getDefault(), "%s %s %s %s",
+                commandInst, commandExtraDebug, commandExtraClass, commandRunner);
+        Log.d(TAG, String.format("The instrument command: %s", command));
+
+        // TODO: 2016/8/23 need root to run instrument command
+        ShellUtils.CommandResult cr = ShellUtils.execCommand(command, true, true);
+        Log.d(TAG, String.format("The instrument test result code: %d", cr.mResult));
+        Log.d(TAG, String.format("The instrument test success message: %s", cr.mSuccessMsg));
+        Log.d(TAG, String.format("The instrument test error message: %s", cr.mErrorMsg));
+
+//        Bundle arguments = new Bundle();
+//        arguments.putString("debug", "false");
+//        arguments.putString("class",
+//                "com.example.zhengjin.funsettingsuitest.testcases.TestPlayingFilm#testDemo");
+//        this.startInstrumentation(new ComponentName(testPackage, testRunner), null, arguments);
     }
 }
