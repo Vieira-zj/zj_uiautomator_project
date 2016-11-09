535c65d2aa142378a1e3ab055a6394e80aa9e3ba
Vieira-zj <zhengjin@fun.tv>
2016-11-09 19:57:21 +0800

    Do refactor.

--- app/src/androidTest/java/com/example/zhengjin/funsettingsuitest/testsuites/RunnerProfile.java
+++ app/src/androidTest/java/com/example/zhengjin/funsettingsuitest/testsuites/RunnerProfile.java
@@ -19,18 +19,17 @@ public final class RunnerProfile {
 
     public static int countAndPrintTestCasesForClass(Class<?> cls) {
         int count = 0;
+        String clsName = cls.getSimpleName();
         Method[] methods = cls.getMethods();
+
         for (Method m : methods) {
             Annotation annotation = m.getAnnotation(org.junit.Test.class);
             if (annotation != null) {
-                Log.d(TAG, String.format(
-                        "Class %s, test case -> %s", cls.getSimpleName(), m.getName()));
+                Log.d(TAG, String.format("Class %s, test case -> %s", clsName, m.getName()));
                 ++count;
             }
         }
-
-        Log.d(TAG, String.format("Class %s, total number of test cases -> %d",
-                cls.getSimpleName(), count));
+        Log.d(TAG, String.format("Class %s, total number of test cases -> %d", clsName, count));
         return count;
     }
 
